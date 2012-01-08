0e7df6f2f5c13cb3a98fa3d86fd25edc0dc282dc
rphilip2004 <rphilip2004@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-08 08:16:44 +0000

    Squashed commit of the following:
    commit 4bde3a55d5f6f138f20c5d5529e9c2b6330711f6
    Author: Phil Chalmers <rphilip.chalmers@gmail.com>
    Date:   Sun Jan 8 03:14:48 2012 -0500
    
        reverted
    
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/faoutlier@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/faoutlier-package.R
@@ -0,0 +1,19 @@
+#' Influential case detection methods for factor analysis and SEM
+#' 
+#' Implements robust Mahalanobis methods, generalized Cook's distances,
+#' likelihood ratio tests, model implied residuals, and various 
+#' graphical methods to help detect and summarize influential 
+#' cases that can affect exploratory and confirmatory factor analyses.  
+#' 
+#'  
+#' 
+#' @name faoutlier-package
+#' @docType package
+#' @title Influential case detection methods for FA and SEM
+#' @author Phil Chalmers \email{rphilip.chalmers@@gmail.com}
+#' @import MASS
+#' @import psych
+# @import OpenMx
+#' @keywords package
+NULL
+
